ad097b530de2a1de5829aba280d16b01987f2f9c
Tofi Buzali <tofi.buzali@gmail.com>
2013-07-11 19:44:52 +0000

    Changed web to read from file index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var bufferSize = 100;
+  var buff = new Buffer(bufferSize);
+  var fd = fs.openSync('index.html','r');
+  var read = fs.readSync(fd, buff, 0, bufferSize);
+  var resp = buff.toString('utf-8',0,read);
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
